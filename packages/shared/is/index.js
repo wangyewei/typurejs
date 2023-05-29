3ecde25c9063339e9cf23aa48842876d66836d18
WangYeWei <826036140@qq.com>
2023-05-29 09:53:10 +0800

    style: Delete excess js files generated by packaging

--- packages/shared/is/index.js
+++
@@ -1,1 +0,0 @@
-export const isString = (val) => typeof val === 'string';
--- packages/typure-cli/src/index.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs-extra';
-import { URL, fileURLToPath } from 'url';
-const packagePath = fileURLToPath(new URL('../package.json', import.meta.url));
-const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf-8'));
-export const cliVersion = packageJson.version;
-export const cliName = packageJson.name;
-process.env.TYPURE_CLI_VERSION = cliVersion;
